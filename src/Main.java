3c74cb371417a327836c763b7c41fb68f54e160e
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-01-13 17:43:12 +0700

    Added employee code input scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
 public class Main {
+	static Scanner scan = new Scanner(System.in);
+	ArrayList<Employee> employees = new ArrayList<>();
+	
 	public Main() {
-		ArrayList<Employee> employees = new ArrayList<>();
+		newKaryawan();
 	}
+	
+	public void newKaryawan() {
+		System.out.println("Input kode karyawan: ");
+		String kode = scan.nextLine();
+	}
+	
 	public static void main(String[] args) {
 		new Main();
 	}
