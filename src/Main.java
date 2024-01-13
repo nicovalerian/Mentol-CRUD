60f8c5baacef9da6f04cdebce375cb935fbae9b0
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-01-13 18:10:51 +0700

    Add employee code validation with regex

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
 	static Scanner scan = new Scanner(System.in);
@@ -10,9 +12,24 @@ public class Main {
 	
 	public void newKaryawan() {
 		Boolean validKode = false, validNama = false, validKelamin = false, validJabatan = false, validGaji = false;
+		String kode, nama, kelamin, jabatan;
+		int gaji;
+		
 		while (!validKode) {
 			System.out.println("Input kode karyawan: ");
-			String kode = scan.nextLine();
+			String kodeTemp = scan.nextLine();
+			// Untuk validasi kode karyawan kita bakal gunakan regex (MM-XXXX) --> "[A-Za-z]+-[0-9]+"
+			Pattern pattern = Pattern.compile("^[A-Za-z]{2}-[0-9]{4}$", Pattern.CASE_INSENSITIVE);
+			/* Menerima hanya string (case insensitive) yang dimulai 2 huruf "MM", 
+			 * diikuti simbol '-',
+			 * dan diakhiri 4 angka numeric "XXXX"
+			 */
+			Matcher matcher = pattern.matcher(kodeTemp);
+			if (matcher.matches()) { // Kalau sesuai pola kode karyawan...
+				
+			} else {
+
+			}
 		}
 		
 	}
